3472d38490ce60b8ae23bdaa93fb08ad1ce39a5e
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-01-29 19:12:25 +0200

    *.BUG-fix style bug

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yardenhendri:Hendri3357@visitbnbdb.ziqqis1.mongodb.net/',
     // dbURL: 'mongodb+srv://tommyirmia:tommy123@mycluster.hvdhdxf.mongodb.net/?retryWrites=true&w=majority',
-    dbName: 'StayBnb_db'
+    dbName: 'VisitBnb_db'
 }
