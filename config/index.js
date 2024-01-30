22f5f6f2120ca0f06e65a8e5479390be330268e0
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-01-30 10:33:03 +0200

    *.BUILD- add dist to backend

--- config/index.js
+++ config/index.js
@@ -5,9 +5,9 @@ import configDev from './dev.js'
 export var config
 
 // for dev - this code doesnt reach
-if (false && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     config = configProd
 } else {
     config = configDev
 }
-// config.isGuestMode = true
+//config.isGuestMode = true
