65d78418efa8f6c62578ff02e7810875e0c5b54f
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-01-31 23:12:05 +0200

    *.build -ver 2

---
+++ public/assets/LazyLoadedImg-sogtjeEw.js
@@ -0,0 +1,1 @@
+import{r as a,j as t}from"./index-ZhhXgfjK.js";const g=({className:s,src:e,alt:n})=>{const[r,m]=a.useState(e);return a.useEffect(()=>{m(e)},[e]),r?t.jsx(t.Fragment,{children:t.jsx("img",{className:s,src:r,alt:"Img"})}):t.jsx(t.Fragment,{})};export{g as default};
