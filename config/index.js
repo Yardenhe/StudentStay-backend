9dbd791ef0d1193194c85cc3c580cb005cd42787
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-06-30 18:12:35 +0300

    *depoly-ver5

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ import configDev from './dev.js'
 export var config
 
 // use flase for dev - this code doesnt reach
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' && false) {
     config = configProd
 } else {
     config = configDev
